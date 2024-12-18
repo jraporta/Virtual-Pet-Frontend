29e4f9d3a49671c06642d0648278240c084b7999
jraporta <jraporta@gmail.com>
2024-12-18 10:58:31 +0100

    redesign home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
+import '../styles/Home.css';
 
 const Home = () => {
-    
     return (
-        <div>
-            <h1>Home page</h1>
+        <div className="home-container">
+            <h1 className="home-title">Welcome to the Virtual Pets Paradise!</h1>
+            <div className="news-section">
+                <h2>Hot News</h2>
+                <ul>
+                    <li>🌟 New pet species added: Mythical Unicorn!</li>
+                    <li>🐾 Check out our Winter Wonderland event, live now!</li>
+                    <li>🎉 Join our community contest to win exclusive prizes!</li>
+                </ul>
+            </div>
+            <div className="banner">
+                🎲 Love games? Try our new Blackjack app and double the fun!
+            </div>
         </div>
     );
 };
 
 export default Home;
-                
