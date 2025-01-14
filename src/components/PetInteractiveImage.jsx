d9a0b911cb4c69a2130d6d4f1ddb00fd8177b37c
jraporta <jraporta@gmail.com>
2025-01-14 10:48:59 +0100

    Use different SVG for the different pets

--- src/components/PetInteractiveImage.jsx
+++ src/components/PetInteractiveImage.jsx
@@ -1,15 +1,29 @@
 import React from "react";
-import PetSVG from '../../public/assets/svg/cat.svg?react';
+import CatSVG from '../../public/assets/svg/cat.svg?react';
+import DogSVG from '../../public/assets/svg/dog.svg?react';
+import HamsterSVG from '../../public/assets/svg/hamster.svg?react';
 import '../styles/PetSVG.css';
 
-const PetInteractiveImage = () => {
+
+const petSVGMap = {
+  CAT: CatSVG,
+  DOG: DogSVG,
+  HAMSTER: HamsterSVG,
+};
+
+const PetInteractiveImage = ({ petType }) => {
+  const SelectedPetSVG = petSVGMap[petType] || null;
 
   return (
-    
+
     <div className="pet-svg-container">
-      <PetSVG />
+      {SelectedPetSVG ? (
+        <SelectedPetSVG />
+      ) : (
+        <p>Pet type not recognized!</p>
+      )}
     </div>
-      
+
   );
 };
 
--- src/services/getPetImage.jsx
+++ src/services/getPetImage.jsx
@@ -1,24 +1,11 @@
 export function getPetImage(species) {
     switch (species) {
         case 'DOG':
-            return "../../public/assets/pet/dog.png";
+            return "/assets/pet/dog.png";
         case 'CAT':
-            return "../../public/assets/pet/cat.png";
+            return "/assets/pet/cat.png";
         case 'HAMSTER':
-            return "../../public/assets/pet/hamster.png";
-        default:
-            return 'data:image/jpeg;base64,...';
-    }
-}
-
-export function getPetSVG(species) {
-    switch (species) {
-        case 'DOG':
-            return "../../public/assets/svg/dog.svg";
-        case 'CAT':
-            return "../../public/assets/svg/cat.svg";
-        case 'HAMSTER':
-            return "../../public/assets/svg/hamster.svg";
+            return "/assets/pet/hamster.png";
         default:
             return 'data:image/jpeg;base64,...';
     }
